025a9433b7e8fefb666201e280e6e97a19a9e11f
Bochesan <bochesan89@yandex.ru>
2019-07-16 18:39:06 +0300

    to order section

--- src/scripts/main/modules/sliders.js
+++ src/scripts/main/modules/sliders.js
@@ -37,34 +37,50 @@ var noveltySlider1 = new Swiper(novelty1,{
       speed: 1500,
     });
 
-    var novelty2 = document.querySelector('.novelty__slider--2');
-    var noveltySlider2 = new Swiper(novelty2,{
-          pagination: {
-            el: '.novelty__slider--2 .swiper-pagination',
-            clickable: true,
-          },
-          effect: 'fade',
-          fadeEffect: {
-            crossFade: true
-          },
-          autoplay: {
-            delay: 2500,
-          },
-          speed: 1500,
-        });
+var novelty2 = document.querySelector('.novelty__slider--2');
+var noveltySlider2 = new Swiper(novelty2,{
+      pagination: {
+        el: '.novelty__slider--2 .swiper-pagination',
+        clickable: true,
+      },
+      effect: 'fade',
+      fadeEffect: {
+        crossFade: true
+      },
+      autoplay: {
+        delay: 2500,
+      },
+      speed: 1500,
+    });
 
-        var novelty3 = document.querySelector('.novelty__slider--3');
-        var noveltySlider3 = new Swiper(novelty3,{
-              pagination: {
-                el: '.novelty__slider--3 .swiper-pagination',
-                clickable: true,
-              },
-              effect: 'fade',
-              fadeEffect: {
-                crossFade: true
-              },
-              autoplay: {
-                delay: 3500,
-              },
-              speed: 1500,
-            });
+var novelty3 = document.querySelector('.novelty__slider--3');
+var noveltySlider3 = new Swiper(novelty3,{
+      pagination: {
+        el: '.novelty__slider--3 .swiper-pagination',
+        clickable: true,
+      },
+      effect: 'fade',
+      fadeEffect: {
+        crossFade: true
+      },
+      autoplay: {
+        delay: 3500,
+      },
+      speed: 1500,
+    });
+
+var order = document.querySelector('.order__slider');
+var orderSlider = new Swiper(order,{
+      pagination: {
+        el: '.order__slider .swiper-pagination',
+        clickable: true,
+      },
+      effect: 'fade',
+      fadeEffect: {
+        crossFade: true
+      },
+      // autoplay: {
+      //   delay: 2500,
+      // },
+      speed: 1500,
+    });
