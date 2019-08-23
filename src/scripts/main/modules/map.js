b3c950f4cc2773eb8b848cb87c5dda3b5ed8d273
Pavel <bochesan89@yandex.ru>
2019-08-24 00:53:38 +0300

    add marker 2GIS

--- src/scripts/main/modules/map.js
+++ src/scripts/main/modules/map.js
@@ -1,11 +1,38 @@
 console.log('map');
-var map;
+// var map
+// myIcon;
+//
+//     DG.then(function () {
+//         map = DG.map('map', {
+//             center: [55.769225568981504,37.583373499999944],
+//             zoom: 16
+//         });
+//
+//         DG.marker([55.769225568981504,37.583373499999944], {
+//                     icon: myIcon
+//                 }).addTo(map).bindPopup('.  Mr.Pont  .');
+//
+//         myIcon = DG.icon({
+//                     iconUrl: 'https://maps.api.2gis.ru/2.0/example_logo.png',
+//                     iconSize: [48, 48]
+//                 });
+//     });
 
-    DG.then(function () {
-        map = DG.map('map', {
-            center: [55.769225568981504,37.583373499999944],
-            zoom: 16
-        });
+DG.then(function() {
+    var map,
+        myIcon;
 
-        DG.marker([55.769225568981504,37.583373499999944]).addTo(map);
+    map = DG.map('map', {
+        center: [55.769225568981504,37.583373499999944],
+        zoom: 16
     });
+
+    myIcon = DG.icon({
+        iconUrl: '/dist/images/skull-ring.png',
+        iconSize: [38, 38]
+    });
+    DG.marker([55.769225568981504,37.583373499999944], {
+        icon: myIcon
+    }).addTo(map).bindPopup('Mr.Pont');
+
+});
