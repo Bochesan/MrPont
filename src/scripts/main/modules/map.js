0d770211f27464e957b24b066507823e4832a8f0
Pavel <bochesan89@yandex.ru>
2019-08-24 00:54:22 +0300

    clean test init map

--- src/scripts/main/modules/map.js
+++ src/scripts/main/modules/map.js
@@ -1,23 +1,3 @@
-console.log('map');
-// var map
-// myIcon;
-//
-//     DG.then(function () {
-//         map = DG.map('map', {
-//             center: [55.769225568981504,37.583373499999944],
-//             zoom: 16
-//         });
-//
-//         DG.marker([55.769225568981504,37.583373499999944], {
-//                     icon: myIcon
-//                 }).addTo(map).bindPopup('.  Mr.Pont  .');
-//
-//         myIcon = DG.icon({
-//                     iconUrl: 'https://maps.api.2gis.ru/2.0/example_logo.png',
-//                     iconSize: [48, 48]
-//                 });
-//     });
-
 DG.then(function() {
     var map,
         myIcon;
