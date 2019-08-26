d10eea76a7617adb2fb44767d3423f0162f2124b
Bochesan <bochesan89@yandex.ru>
2019-08-26 11:11:56 +0300

    page 404

---
+++ page-404.php
@@ -0,0 +1,20 @@
+<? require_once('header.php'); ?>
+
+<main>
+    <div class="wrapper nf">
+        <div class="breadcrumbs">
+            <ul class="breadcrumbs__container">
+                <li class="breadcrumbs__item"><a href="/" class="breadcrumbs__link">Главная</a></li>
+                <li class="breadcrumbs__item">Страницы не существует</li>
+            </ul>
+        </div>
+        <div class="nf__container">
+            <div class="nf__title">404</div>
+            <div class="nf__text">Похоже, Вы<br> свернули не туда</div>
+            <a href="/" class="button">Назад</a>
+        </div>
+    </div>
+
+</main>
+
+<? require('footer.php'); ?>
