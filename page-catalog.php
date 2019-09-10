f48374fb29846cd6f57fa4b5a86fe9b51c981177
Pavel <bochesan89@yandex.ru>
2019-09-10 22:42:54 +0300

    catalog image

--- page-catalog.php
+++ page-catalog.php
@@ -16,41 +16,196 @@
 
     <div class="catalog wrapper">
 
-        <div class="catalog__box fadeIn">
+        <div class="catalog__box fadeIn shadow-name">
             <a href="/page-detail-catalog.php" class="catalog__item">
+                <div class="shadow-name__title">Кольца <br>и перстни</div>
                 <div class="catalog__image">
-                    <img src="/dist/images/ring.png" alt="">
+                    <img src="/dist/images/catalog/img1.png" alt="">
                 </div>
                 <div class="catalog__title">Кольца и перстни</div>
             </a>
         </div>
 
-        <div class="catalog__box fadeIn">
+        <div class="catalog__box fadeIn shadow-name">
             <a href="/page-detail-catalog.php" class="catalog__item">
+                <div class="shadow-name__title">Серьги</div>
                 <div class="catalog__image">
-                    <img src="/dist/images/ring.png" alt="">
+                    <img src="/dist/images/catalog/img2.png" alt="">
                 </div>
-                <div class="catalog__title">Кольца и перстни</div>
+                <div class="catalog__title">Серьги</div>
             </a>
         </div>
 
-        <div class="catalog__box fadeIn">
+        <div class="catalog__box fadeIn shadow-name">
             <a href="/page-detail-catalog.php" class="catalog__item">
+                <div class="shadow-name__title">Каффы</div>
                 <div class="catalog__image">
-                    <img src="/dist/images/ring.png" alt="">
+                    <img src="/dist/images/catalog/img3.png" alt="">
                 </div>
-                <div class="catalog__title">Кольца и перстни</div>
+                <div class="catalog__title">Каффы</div>
             </a>
         </div>
 
-        <div class="catalog__box fadeIn">
+        <div class="catalog__box fadeIn shadow-name">
             <a href="/page-detail-catalog.php" class="catalog__item">
+                <div class="shadow-name__title">Запонки</div>
                 <div class="catalog__image">
-                    <img src="/dist/images/ring.png" alt="">
+                    <img src="/dist/images/catalog/img4.png" alt="">
                 </div>
-                <div class="catalog__title">Кольца и перстни</div>
+                <div class="catalog__title">Запонки</div>
             </a>
         </div>
+
+        <div class="catalog__box fadeIn shadow-name">
+            <a href="/page-detail-catalog.php" class="catalog__item">
+                <div class="shadow-name__title">Прядки <br>к ремням</div>
+                <div class="catalog__image">
+                    <img src="/dist/images/catalog/img5.png" alt="">
+                </div>
+                <div class="catalog__title">Прядки к ремням</div>
+            </a>
+        </div>
+
+        <div class="catalog__box fadeIn shadow-name">
+            <a href="/page-detail-catalog.php" class="catalog__item">
+                <div class="shadow-name__title">Темляки <br>для ножей</div>
+                <div class="catalog__image">
+                    <img src="/dist/images/catalog/img6.png" alt="">
+                </div>
+                <div class="catalog__title">Темляки для ножей</div>
+            </a>
+        </div>
+
+        <div class="catalog__box fadeIn shadow-name">
+            <a href="/page-detail-catalog.php" class="catalog__item">
+                <div class="shadow-name__title">Трости</div>
+                <div class="catalog__image">
+                    <img src="/dist/images/catalog/img7.png" alt="">
+                </div>
+                <div class="catalog__title">Трости</div>
+            </a>
+        </div>
+
+        <div class="catalog__box fadeIn shadow-name">
+            <a href="/page-detail-catalog.php" class="catalog__item">
+                <div class="shadow-name__title">Зажимы <br>для купюр</div>
+                <div class="catalog__image">
+                    <img src="/dist/images/catalog/img8.png" alt="">
+                </div>
+                <div class="catalog__title">Зажимы для купюр</div>
+            </a>
+        </div>
+
+        <div class="catalog__box fadeIn shadow-name">
+            <a href="/page-detail-catalog.php" class="catalog__item">
+                <div class="shadow-name__title">Зажимы для<br> галстука</div>
+                <div class="catalog__image">
+                    <img src="/dist/images/catalog/img9.png" alt="">
+                </div>
+                <div class="catalog__title">Зажимы для галстука</div>
+            </a>
+        </div>
+
+        <div class="catalog__box fadeIn shadow-name">
+            <a href="/page-detail-catalog.php" class="catalog__item">
+                <div class="shadow-name__title">Браслеты</div>
+                <div class="catalog__image">
+                    <img src="/dist/images/catalog/img10.png" alt="">
+                </div>
+                <div class="catalog__title">Браслеты</div>
+            </a>
+        </div>
+
+        <div class="catalog__box fadeIn shadow-name">
+            <a href="/page-detail-catalog.php" class="catalog__item">
+                <div class="shadow-name__title">Подвески <br>и кулоны</div>
+                <div class="catalog__image">
+                    <img src="/dist/images/catalog/img11.png" alt="">
+                </div>
+                <div class="catalog__title">Подвески и кулоны</div>
+            </a>
+        </div>
+
+        <div class="catalog__box fadeIn shadow-name">
+            <a href="/page-detail-catalog.php" class="catalog__item">
+                <div class="shadow-name__title">Брелки</div>
+                <div class="catalog__image">
+                    <img src="/dist/images/catalog/img12.png" alt="">
+                </div>
+                <div class="catalog__title">Брелки</div>
+            </a>
+        </div>
+
+        <div class="catalog__box fadeIn shadow-name">
+            <a href="/page-detail-catalog.php" class="catalog__item">
+                <div class="shadow-name__title">Авторучки <br>серебряные</div>
+                <div class="catalog__image">
+                    <img src="/dist/images/catalog/img13.png" alt="">
+                </div>
+                <div class="catalog__title">Авторучки серебряные</div>
+            </a>
+        </div>
+
+        <div class="catalog__box fadeIn shadow-name">
+            <a href="/page-detail-catalog.php" class="catalog__item">
+                <div class="shadow-name__title">Зажигалки</div>
+                <div class="catalog__image">
+                    <img src="/dist/images/catalog/img14.png" alt="">
+                </div>
+                <div class="catalog__title">Зажигалки</div>
+            </a>
+        </div>
+
+        <div class="catalog__box fadeIn shadow-name">
+            <a href="/page-detail-catalog.php" class="catalog__item">
+                <div class="shadow-name__title">Пепельницы</div>
+                <div class="catalog__image">
+                    <img src="/dist/images/catalog/img15.png" alt="">
+                </div>
+                <div class="catalog__title">Пепельницы</div>
+            </a>
+        </div>
+
+        <div class="catalog__box fadeIn shadow-name">
+            <a href="/page-detail-catalog.php" class="catalog__item">
+                <div class="shadow-name__title">Изделия <br>из кожи</div>
+                <div class="catalog__image">
+                    <img src="/dist/images/catalog/img16.png" alt="">
+                </div>
+                <div class="catalog__title">Изделия из кожи</div>
+            </a>
+        </div>
+
+        <div class="catalog__box fadeIn shadow-name">
+            <a href="/page-detail-catalog.php" class="catalog__item">
+                <div class="shadow-name__title">Чехлы для<br> телефонов</div>
+                <div class="catalog__image">
+                    <img src="/dist/images/catalog/img17.png" alt="">
+                </div>
+                <div class="catalog__title">Чехлы для телефонов из кожи и серебра</div>
+            </a>
+        </div>
+
+        <div class="catalog__box fadeIn shadow-name">
+            <a href="/page-detail-catalog.php" class="catalog__item">
+                <div class="shadow-name__title">Золотые <br>изделия</div>
+                <div class="catalog__image">
+                    <img src="/dist/images/catalog/img18.png" alt="">
+                </div>
+                <div class="catalog__title">Золотые изделия</div>
+            </a>
+        </div>
+
+        <div class="catalog__box fadeIn shadow-name">
+            <a href="/page-detail-catalog.php" class="catalog__item">
+                <div class="shadow-name__title">Test</div>
+                <div class="catalog__image">
+                    <img src="/dist/images/catalog/img1.jpg" alt="">
+                </div>
+                <div class="catalog__title">Test</div>
+            </a>
+        </div>
+
 
     </div>
 
